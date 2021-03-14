0b5493e5c6d006c5fa5c783b2e423d8f87f6c8a1
Derek Zhang <derek.zhang0210@gmail.com>
2021-03-14 04:25:10 -0400

    added material ui

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import {useState} from 'react'
+import { useState } from 'react'
+import { Button } from '@material-ui/core'
 
 export default function Home() {
 
@@ -20,7 +21,7 @@ export default function Home() {
             <h1>P I X E L P A L E T T E </h1>
 
             <input type="file" id="upload"  onChange={uploadHandler} style={{display:"none"}}/>
-            <button> <label for="upload">Select file</label> </button>
+            <Button color="primary"> <label for="upload">Select file</label> </Button>
             <img src={img} width="500"/>
         </div>
     )

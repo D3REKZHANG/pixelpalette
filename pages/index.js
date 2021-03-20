68f16501c406156d9c95aa684901136bf4a99d19
Derek Zhang <derek.zhang0210@gmail.com>
2021-03-19 23:54:20 -0400

    added canvas

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { Button } from '@material-ui/core'
 
 export default function Home() {
 
     const [img, setImg] = useState();
+    const canvas = useRef(null);
 
     const uploadHandler = (event) =>{
         setImg(URL.createObjectURL(event.target.files[0]));
     }
+
+    useEffect(()=>{
+        const ctx = canvas.current.getContext();
+    },[img]);
 
     return (
         <div className={styles.container}>
@@ -23,6 +28,7 @@ export default function Home() {
             <input type="file" id="upload"  onChange={uploadHandler} style={{display:"none"}}/>
             <Button color="primary"> <label for="upload">Select file</label> </Button>
             <img src={img} width="500"/>
+            <canvas id="display" width="200" height="100" style={{border:"1px solid #000000"}} ref={canvas}></canvas>
         </div>
     )
 }

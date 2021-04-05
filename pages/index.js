2b7198e7d83fd5117db4294b2ba2af780bfaa150
Derek Zhang <derek.zhang0210@gmail.com>
2021-04-04 23:30:48 -0400

    fixed bugs and has working upload to canvas

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,50 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { useState, useRef } from 'react'
-import { Button } from '@material-ui/core'
+import { useState, useRef, useEffect } from 'react'
+import { Button, Slider, Grid, Input } from '@material-ui/core'
+import Typography from '@material-ui/core/Typography';
+
+import { scaleRes } from "../processing.js";
 
 export default function Home() {
 
-    const [img, setImg] = useState();
+    const [image, setImage] = useState(null);
+    const [imageData, setImageData] = useState(null);
+
+    // options
+    const [res, setRes] = useState(100);
+
     const canvas = useRef(null);
 
-    const uploadHandler = (event) =>{
-        setImg(URL.createObjectURL(event.target.files[0]));
-    }
+    const uploadHandler = (e) =>{
+        const reader = new FileReader();
+        reader.onload = () => {
+            if(reader.readyState === 2){
+                const img = new Image;
+                img.src = reader.result;
+                img.onload = () =>{
+                    setImage(img);
+                }
+            }
+        }
+        reader.readAsDataURL(e.target.files[0])
+    };
 
     useEffect(()=>{
-        const ctx = canvas.current.getContext();
-    },[img]);
+        if(image === null) return;
+
+        const ctx = canvas.current.getContext('2d');
+        ctx.drawImage(image,0,0,500,image.height*(500/image.width));
+        const imgData = ctx.getImageData(0,0,500,image.height*(500/image.width));
+        setImageData(imgData);
+        scaleRes(imgData);
+    },[image]);
+
+    useEffect(()=>{
+        if(image == null) return;
+        const ctx = canvas.current.getContext('2d');
+        ctx.putImageData(scaleRes(imageData, res),0,0);
+    },[res]);
 
     return (
         <div className={styles.container}>
@@ -24,11 +54,33 @@ export default function Home() {
             </Head>
 
             <h1>P I X E L P A L E T T E </h1>
-
-            <input type="file" id="upload"  onChange={uploadHandler} style={{display:"none"}}/>
+            <input type="file" id="upload" accept="image/*"onChange={uploadHandler} style={{display:"none"}}/>
             <Button color="primary"> <label for="upload">Select file</label> </Button>
-            <img src={img} width="500"/>
-            <canvas id="display" width="200" height="100" style={{border:"1px solid #000000"}} ref={canvas}></canvas>
+            <canvas id="display" width={(image===null)?0:500} height={(image === null)?0:image.height*(500/image.width)} style={{border:((image==null)?0:1)+"px solid #000000"}} ref={canvas}></canvas>
+
+            <div style={{visibility:(image===null)?"hidden":"visible", width:"350px", margin:"30px"}}>
+                <Grid container spacing={2} alignItems="center">
+                    <Grid item>
+                        <Typography> Resolution </Typography>
+                    </Grid>
+                    <Grid item xs>
+                        <Slider
+                            value={typeof res === 'number' ? res : 0}
+                            onChange={(e, newval) => setRes(newval)}
+                            aria-labelledby="input-slider"
+                            defaultValue = {100} step = {1} min={1} max={100}
+                        />
+                    </Grid>
+                    <Grid item>
+                        <Input
+                            style={{width: 30}}
+                            value={res}
+                            margin="dense"
+                            onChange={(e)=>setRes(e.target.value === '' ? '' : Number(e.target.value.slice(0,-1)))}
+                        />
+                    </Grid>
+                </Grid>
+            </div>
         </div>
     )
 }

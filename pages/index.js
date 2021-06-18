15b76d6788589cca2d2a79225f40ba3431ef9094
Derek Zhang <derek.zhang0210@gmail.com>
2021-06-18 10:17:30 -0400

    Add Block/Average scale option, add settings dialog

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,30 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { useState, useRef, useEffect } from 'react'
-import { Button, Slider, Grid, Input } from '@material-ui/core'
+import { DialogContent, DialogActions, DialogTitle, Dialog, IconButton, MenuItem, Select, Button, Slider, Grid, Input } from '@material-ui/core'
+import SettingsIcon from '@material-ui/icons/Settings';
 import Typography from '@material-ui/core/Typography';
 
-import { scaleRes } from "../processing.js";
+import { scalePixels } from "../processing.js";
 
 export default function Home() {
 
+    // STATES AND REFS
     const [image, setImage] = useState(null);
     const [imageData, setImageData] = useState(null);
 
+    const [settingsOpen, setSettingsOpen] = useState(false);
+
     // options
+    const [scaleStyle, setScaleStyle] = useState("block");
     const [scale, setScale] = useState(1);
 
     const canvas = useRef(null);
 
-    const uploadHandler = (e) =>{
+    
+    // EVENT HANDLERS
+    
+    const handleUpload = (e) => {
         const reader = new FileReader();
         reader.onload = () => {
             if(reader.readyState === 2){
@@ -30,6 +38,14 @@ export default function Home() {
         reader.readAsDataURL(e.target.files[0])
     };
 
+    const handleStyleChange = (e) => setScaleStyle(e.target.value);
+
+    const handleSettingsClick = (e) => setSettingsOpen(true);
+    const handleSettingsClose = (e) => setSettingsOpen(false);
+
+
+    // USE EFFECT HOOKS
+    
     useEffect(()=>{
         if(image === null) return;
 
@@ -47,8 +63,9 @@ export default function Home() {
     useEffect(()=>{
         if(image == null) return;
         const ctx = canvas.current.getContext('2d');
-        ctx.putImageData(scaleRes(ctx, imageData, scale),0,0);
+        ctx.putImageData(scalePixels(ctx, imageData, scale, scaleStyle),0,0);
     },[scale]);
+
 
     return (
         <div className={styles.container}>
@@ -58,7 +75,7 @@ export default function Home() {
             </Head>
 
             <h1>P I X E L P A L E T T E </h1>
-            <input type="file" id="upload" accept="image/*"onChange={uploadHandler} style={{display:"none"}}/>
+            <input type="file" id="upload" accept="image/*"onChange={handleUpload} style={{display:"none"}}/>
             <Button color="primary"> <label for="upload">Select file</label> </Button>
             <canvas id="display" width={(image===null)?0:500} height={(image === null)?0:image.height*(500/image.width)} style={{border:((image==null)?0:1)+"px solid #000000"}} ref={canvas}></canvas>
 
@@ -74,11 +91,31 @@ export default function Home() {
                             aria-labelledby="discrete-slider"
                             valueLabelDisplay="auto"
                             marks
-                            defaultValue = {1} step = {1} min={1} max={6}
+                            defaultValue = {1} step = {1} min={1} max={10}
                         />
+                    </Grid>
+                    <Grid item>
+                        <IconButton color="primary" onClick={handleSettingsClick}> <SettingsIcon /> </IconButton>
                     </Grid>
                 </Grid>
             </div>
+            <Dialog disableBackdropClick disableEscapeKeyDown open={settingsOpen}>
+                <DialogTitle>Settings</DialogTitle>
+                <DialogContent>
+                    <Select
+                        value={scaleStyle}
+                        onChange={handleStyleChange}
+                    > 
+                        <MenuItem value={"block"}>Block</MenuItem>
+                        <MenuItem value={"average"}>Average</MenuItem>
+                    </Select>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleSettingsClose} color="primary">
+                        Ok
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     )
 }

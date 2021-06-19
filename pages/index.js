bbf7d214b947fd3453d8df7a15608ec77ee28ccd
Derek Zhang <derek.zhang0210@gmail.com>
2021-06-19 00:18:55 -0400

    Add caching

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { useState, useRef, useEffect } from 'react'
-import { DialogContent, DialogActions, DialogTitle, Dialog, IconButton, MenuItem, Select, Button, Slider, Grid, Input } from '@material-ui/core'
+import { CircularProgress, Box, DialogContent, DialogActions, DialogTitle, Dialog, IconButton, MenuItem, Select, Button, Slider, Grid, Input } from '@material-ui/core'
 import SettingsIcon from '@material-ui/icons/Settings';
 import Typography from '@material-ui/core/Typography';
+import LoadingOverlay from 'react-loading-overlay';
 
 import { scalePixels } from "../processing.js";
 
@@ -12,11 +13,13 @@ export default function Home() {
     // STATES AND REFS
     const [image, setImage] = useState(null);
     const [imageData, setImageData] = useState(null);
+    const [cache, setCache] = useState([]);
 
     const [settingsOpen, setSettingsOpen] = useState(false);
+    var loading = false;
 
     // options
-    const [scaleStyle, setScaleStyle] = useState("block");
+    const [scaleStyle, setScaleStyle] = useState(0);
     const [scale, setScale] = useState(1);
 
     const canvas = useRef(null);
@@ -48,24 +51,32 @@ export default function Home() {
     
     useEffect(()=>{
         if(image === null) return;
-
         const ctx = canvas.current.getContext('2d');
         ctx.drawImage(image,0,0,500,image.height*(500/image.width));
         const imgData = ctx.getImageData(0,0,500,image.height*(500/image.width));
         setImageData(imgData);
-        console.log("yert");
+
+        loading = true;
+        // cache 
+        const arr = [[0], [0]];
+        for (var i=1;i<=10;i++) {
+            arr[0].push(scalePixels(ctx, imgData, i, "block"));
+            arr[1].push(scalePixels(ctx, imgData, i, "average"));
+        }
+        loading = false;
+        setCache(arr);
     },[image]);
 
     useEffect(()=>{
-        console.log("changed");
-    }, [imageData]);
-
-    useEffect(()=>{
-        if(image == null) return;
+        if(image === null) return;
         const ctx = canvas.current.getContext('2d');
-        ctx.putImageData(scalePixels(ctx, imageData, scale, scaleStyle),0,0);
-    },[scale]);
+        ctx.putImageData(cache[scaleStyle][scale],0,0);
+    },[scale, scaleStyle]);
 
+    
+    const cacheData = (imgData, style) => {
+        const ctx = canvas.current.getContext('2d');
+    }
 
     return (
         <div className={styles.container}>
@@ -77,7 +88,18 @@ export default function Home() {
             <h1>P I X E L P A L E T T E </h1>
             <input type="file" id="upload" accept="image/*"onChange={handleUpload} style={{display:"none"}}/>
             <Button color="primary"> <label for="upload">Select file</label> </Button>
-            <canvas id="display" width={(image===null)?0:500} height={(image === null)?0:image.height*(500/image.width)} style={{border:((image==null)?0:1)+"px solid #000000"}} ref={canvas}></canvas>
+            <LoadingOverlay
+                active={loading}
+                spinner={<CircularProgress />}
+                styles={{
+                    overlay: (base) => ({
+                        ...base,
+                        background: 'rgba(0, 0, 0, 0.3)'
+                    })
+                }}
+            >
+                <canvas id="display" width={(image===null)?0:500} height={(image === null)?0:image.height*(500/image.width)} style={{border:((image==null)?0:1)+"px solid #000000"}} ref={canvas}></canvas>
+            </LoadingOverlay>
 
             <div style={{visibility:(image===null)?"hidden":"visible", width:"350px", margin:"30px"}}>
                 <Grid container spacing={2} alignItems="center">
@@ -99,16 +121,27 @@ export default function Home() {
                     </Grid>
                 </Grid>
             </div>
-            <Dialog disableBackdropClick disableEscapeKeyDown open={settingsOpen}>
+            <Dialog
+                disableBackdropClick
+                disableEscapeKeyDown
+                open={settingsOpen}
+            >
                 <DialogTitle>Settings</DialogTitle>
                 <DialogContent>
-                    <Select
-                        value={scaleStyle}
-                        onChange={handleStyleChange}
-                    > 
-                        <MenuItem value={"block"}>Block</MenuItem>
-                        <MenuItem value={"average"}>Average</MenuItem>
-                    </Select>
+                    <Grid container spacing={2} alignItems="center" justify="space-between">
+                        <Grid item>
+                            <Typography>Style</Typography>
+                        </Grid>
+                        <Grid item>
+                            <Select
+                                value={scaleStyle}
+                                onChange={handleStyleChange}
+                            > 
+                                <MenuItem value={0}>Sharp</MenuItem>
+                                <MenuItem value={1}>Soft</MenuItem>
+                            </Select>
+                        </Grid>
+                    </Grid>
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleSettingsClose} color="primary">
